8a4812607b1cab42a879a960bcbb20cf9168e3f6
graham-ryan <grahamryan2000@gmail.com>
2021-01-17 14:23:10 -0500

    ~emojitoimage now works for default emojis. Program now uses relative paths.

--- FlowerBot.py
+++ FlowerBot.py
@@ -6,6 +6,8 @@ from image_tools import emoji_tools
 # Choose bot command prefix
 bot = commands.Bot(command_prefix = '~')
 
+dirname = os.path.dirname(__file__)
+
 @bot.event
 async def on_ready():
     print(f'Logged in as {bot.user}')
@@ -17,7 +19,7 @@ async def gimmethat(ctx):
         message = ctx.message.reference.resolved # Gets the referenced message
         if (not message.attachments):
             await ctx.send('This isn\'t an image.')
-        elif not (message.attachments[0].filename.endswith('.jpg') or message.attachments[0].filename.endswith('.png')):
+        elif not (message.attachments[0].filename.endswith('.jpg') or message.attachments[0].filename.endswith('.png') or message.attachments[0].filename.endswith('.jpeg')):
             await ctx.send('This isn\'t an image.')
         else:
            f = await message.attachments[0].to_file()
@@ -25,27 +27,34 @@ async def gimmethat(ctx):
     else:
         await ctx.send('You have to reference something for me to give you it.')
 
-# Sends a wolf to the server
+# Sends a picture to the server from this folder called pic.png
 @bot.command()
 async def sendpics(ctx):
-    f = discord.File("C:/Users/graha/OneDrive/Pictures/wolfs.jpg", filename="wolfys.jpg")
+    f = discord.File(f"{dirname}/pic.png")
     await ctx.send(file=f) 
 
 # Sends the emojis you sent back as an image
 @bot.command()
 async def emojitoimage(ctx, *args : str):
-    urls = []
+    # List of emote pics
+    emotes = []
     # Loop through each arg
     for arg in args:
         # Try converting to Emoji object:
         try:
-            Converter = commands.EmojiConverter()
+            Converter = commands.PartialEmojiConverter()
             emote = await Converter.convert(ctx=ctx,argument=arg)
             await ctx.send(emote.url)
-            urls.append(emote.url)
-        except commands.EmojiNotFound:
-            await ctx.send(f'\'{arg}\' is not an emote.')   
-    print(urls)        
+            emotes.append(emote.url)
+        except (commands.EmojiNotFound, commands.errors.PartialEmojiConversionFailure):
+            # Try to find emoji
+            codePoint = emoji_tools.findCodePoint(arg)
+            if (codePoint != False):
+                # Get the image and send it
+                f = discord.File(f"{dirname}/72x72/{codePoint}.png")
+                await ctx.send(file=f)              
+            else:
+                await ctx.send(f'\'{arg}\' is not an emote.')
 
 # Put the bot's client secret key here
-bot.run('')
+bot.run('Nzk4NjY4OTUyOTMzNzYxMDQ0.X_4YbA.4P0i8MdDL8mXXSoEAoZ9KW_YH2w')
--- image_tools/emoji_tools.py
+++ image_tools/emoji_tools.py
@@ -1,3 +1,29 @@
+# Assorted methods to help convert emojis into images, and paste them onto images
 import PIL.Image
+import os
 
+# Find Code Point for Emoji. Returns false if there isn't one.
+def findCodePoint(arg: str):
+    line = findAssociatedLine(arg)
+    if (line == False):
+        return False
+    else:
+        # Make line into just a substring
+        line = line[0:19].strip().replace(" ","-")
+        return(line)
 
+# Returns the line associated with the Emoji passed to this function
+def findAssociatedLine(arg: str):
+    # ToDo: Make this path relative
+    dirname = os.path.dirname(__file__)
+    file = open(f"{dirname}\\emoji-mapping.txt","r",encoding="utf8")
+    for line in file:
+        if arg in line:
+            ans = line
+            file.close()
+            print(ans)
+            return(ans)      
+    return False
+
+def imagePaste():
+    pass
