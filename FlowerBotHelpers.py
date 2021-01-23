5c43e079dfc0f8e5607fb6700387a22b3a62004b
graham-ryan <grahamryan2000@gmail.com>
2021-01-22 23:57:06 -0500

    Border chaos now functions! Commands can reply to embeds!

---
+++ FlowerBotHelpers.py
@@ -0,0 +1,45 @@
+# Some helper functions for FlowerBot.py
+import discord
+import io
+import aiohttp
+from PIL import Image, ImageDraw
+
+# Checks if the message sent is replying to a message with an image either attached, embedded, or has a link to. If it does, return it as an image.
+async def getReplyingImage(ctx):
+     if ctx.message.reference is not None: 
+          # Get the referenced message
+          message = ctx.message.reference.resolved 
+          if ((len(message.attachments)>0) and (message.attachments[0].filename.endswith('.jpg') or message.attachments[0].filename.endswith('.jpeg') or message.attachments[0].filename.endswith('.png'))):
+               # If message content contains an attachment with an image
+               return await urlToImage(message.attachments[0].url)
+          elif (message.content.endswith('.jpg') or message.content.endswith('.jpeg') or message.content.endswith('.png')):
+               # If message content contains a link with an image
+               return await urlToImage(message.content)
+          elif (len(message.embeds)>0):
+               # If message content has an embed, check if there is an image
+               if (message.embeds[0].thumbnail != message.embeds[0].Empty):
+                    return await urlToImage(message.embeds[0].thumbnail.url)
+          else:
+               return None
+     else:
+          return None
+
+# Given a Discordpy Asset object, returns a PIL image object
+async def assetToImage(asset: discord.Asset) -> Image:
+     imageBytes = io.BytesIO(await asset.read())
+     return Image.open(imageBytes)
+
+# Given a url string, returns a PIL image object. Returns none if something fails.
+async def urlToImage(url: str):
+     try:
+          # Check if URL works
+          connector = aiohttp.ClientSession()
+          resp = await connector.get(url)
+          buffer = io.BytesIO(await resp.read())
+          img = Image.open(buffer)
+          await connector.close()
+          return img
+     except Exception:
+          # Something failed along the way                       
+          await connector.close()
+          return None
--- README.md
+++ README.md
@@ -1,6 +1,19 @@
 # FlowerBot
 A Discord bot to quickly edit pictures by layering emotes on them.
+
 Emotes can be either the default unicode ones, or custom made from Discord servers.
 
-Assets for default emotes comes from twemoji https://github.com/twitter/twemoji 
-Translation / mapping to codepoint from utf-8 from https://unicode.org/Public/emoji/13.1/emoji-test.txt
+Bot is written using the [DiscordPy API](https://discordpy.readthedocs.io/en/latest/index.html#)
+
+Assets for default emotes comes from [Twemoji](https://github.com/twitter/twemoji) 
+
+Translation / mapping to codepoint comes from UTF-8 [Unicode's Website](https://unicode.org/Public/emoji/13.1/emoji-test.txt)
+
+# Commands
+Command prefix is by default '~'
+
+* `border [Light/Normal/Chaos] <Emote>...` - In reply to another Discord message with an image attached or embedded, sends an image back with the emotes pasted along the border of it. Number of emotes and spacing based on the choice of Light / Normal / Chaos.
+
+* `emojitoimage <Emote>...` - Given any/all emotes in the command line, sends images of all the emotes back into the text channel
+
+* `gimmethat` - In reply to another Discord message with an image attached or embedded, resends the image into the text channel
